7446f040a24a4065930ef014a9abd6a74bede414
mandipp <mandippokharel@outlook.com>
2016-03-09 23:35:19 -0500

    First attempt. Not checked

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
 // Your JavaScript goes here...
+
+//Creating the request
+request = new XMLHttpRequest();
+
+request.open("GET", "data.json", true);
+
+request.onreadystatechange = callme;
+
+request.send(null);
+
+function callme {
+	if (request.readystate == 4) {
+		content = "";
+		element = document.getElementById("messages")
+		raw = request.responseText;
+		messages = JSON.parse(raw);
+		for (i=0; i<messages.length; i++) {
+			content += "<p>" + messages[i].content'<span class = "author">' + messages[i].username + "</span></p>"
+		}
+		element.innerHTML = content;
+	}
+	else {
+		document.getElementById.innerHTML = "Fail!";
+	}
+}
