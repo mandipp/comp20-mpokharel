6f3cb718e5d8b102bd424d4d6b5ba9c00b038f9d
mandipp <mandippokharel@outlook.com>
2016-03-10 00:52:22 -0500

    Started working. Some work required

--- messages/lab.js
+++ messages/lab.js
@@ -1,26 +1,26 @@
-// Your JavaScript goes here...
-
+function parse () {
 //Creating the request
-request = new XMLHttpRequest();
+	request = new XMLHttpRequest();
 
-request.open("GET", "data.json", true);
+	request.open("GET", "data.json", true);
+	
+	request.onreadystatechange = callme;
 
-request.onreadystatechange = callme;
+	request.send(null);
 
-request.send(null);
-
-function callme {
-	if (request.readystate == 4) {
-		content = "";
-		element = document.getElementById("messages")
-		raw = request.responseText;
-		messages = JSON.parse(raw);
-		for (i=0; i<messages.length; i++) {
-			content += "<p>" + messages[i].content'<span class = "author">' + messages[i].username + "</span></p>"
+	function callme () {
+		if (request.readyState === 4) {
+			content = "";
+			element = document.getElementById("messages");
+			raw = request.responseText;
+			msg = JSON.parse(raw);
+			for (i = 0; i < msg.length; i++) {
+				content += "<p>" + msg[i].content + '<span class = "author">' + msg[i].username + "</span></p>";
+			}
+			element.innerHTML = content;
 		}
-		element.innerHTML = content;
-	}
-	else {
-		document.getElementById.innerHTML = "Fail!";
+		else {
+			document.getElementById("messages").innerHTML = "Fail!";
+		}
 	}
 }
